95f4a5fec65d961f935d25e888c039411c018be8
emericsoquet <soquet.emeric@gmail.com>
2022-08-03 18:52:53 +0200

    Gestion des images animées

--- src/Views/Pokemon.jsx
+++ src/Views/Pokemon.jsx
@@ -26,8 +26,14 @@ const Pokemon = () => {
 
             <figure className={`${ styles.pokemon__img } col-4 mx-auto`}>
               { pokemon.sprites &&
-                  <img src={ pokemon.sprites.versions['generation-v']['black-white'].animated.front_default } />
+                  <img src={ 
+                              pokemon.sprites.versions['generation-v']['black-white'].animated.front_default != null ? 
+                              pokemon.sprites.versions['generation-v']['black-white'].animated.front_default : 
+                              pokemon.sprites.front_default  } 
+                  />
+                  
               }
+              { console.log(pokemon.sprites)}
             </figure>
 
             <div className={`${ styles.pokemon__body }`} >
