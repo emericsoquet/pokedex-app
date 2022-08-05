7847f4a72f492fdb4e33b66001129b6a904e9b66
emericsoquet <soquet.emeric@gmail.com>
2022-08-05 12:56:15 +0200

    Composant metas + style

---
+++ src/Components/Metas/Metas.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styles from '../../Styles/Pokemon.module.scss'
+import { v4 as uuidv4 } from 'uuid'
+
+const Metas = ({pokemon, pokemonDetails}) => {
+
+    return (
+        <>
+            <div className={`container ${ styles.pokemon__metas }`}>
+
+                <ul className="row col-xl-10 col-xxl-8 mx-auto">
+                    <li className={`${ styles.meta } col-12 col-md-6 col-xl-4`}>
+                        <span className={ styles.meta__property }>Height</span>
+                        <span className={ styles.meta__value }>{ (Math.round(pokemon.height * 10) / 100).toFixed(2) } m</span>
+                    </li>
+                    <li className={`${ styles.meta } col-12 col-md-6 col-xl-4`}>
+                        <span className={ styles.meta__property }>Weight</span>
+                        <span className={ styles.meta__value }>{ (Math.round(pokemon.weight * 10) / 100).toFixed(2) } kg</span>
+                    </li>
+                    <li className={`${ styles.meta } col-12 col-md-6 col-xl-4`}>
+                        <span className={ styles.meta__property }>Catch rate</span>
+                        <span className={ styles.meta__value }>{ pokemonDetails.capture_rate }</span>
+                    </li>
+                    <li className={`${ styles.meta } col-12 col-md-6 col-xl-4`}>
+                        <span className={ styles.meta__property }>Gender</span>
+                        { pokemonDetails.gender_rate === -1 ? 
+                            <span className={ styles.meta__value }>Unknown</span> :
+                            <>
+                                <span className={ styles.meta__value }>{ (pokemonDetails.gender_rate / 8) * 100 }% ♀ </span>
+                                <span className={ styles.meta__value }>{ ((8 - pokemonDetails.gender_rate) / 8) * 100 }% ♂</span>  
+                            </>
+                        }
+                    </li>
+                    <li className={`${ styles.meta } col-12 col-md-6 col-xl-4`}>
+                        <span className={ styles.meta__property }>Egg group</span>
+                        <span className={ styles.meta__value }>
+                            { 
+                                pokemonDetails.egg_groups?.map( (group) => {
+                                    return <span key={ uuidv4() } className={`${ styles.meta__value } ${ styles.meta__multiple}` }>{ group.name }</span>
+                                })
+                            } 
+                        </span>
+                    </li>
+                    <li className={`${ styles.meta } col-12 col-md-6 col-xl-4`}>
+                        <span className={ styles.meta__property }>Hatch counter</span>
+                        <span className={ styles.meta__value }>{ pokemonDetails.hatch_counter } cycles</span>
+                    </li>
+                    <li className={`${ styles.meta } col-12`}>
+                        <span className={ styles.meta__property }>Abilities</span>
+                        { pokemon.abilities?.map( (ability, index) =>
+                            <span className={`${ styles.meta__value } ${ styles.meta__multiple}`} key={ uuidv4() }>{ ability.ability.name }</span>
+                        )}
+                    </li>
+                </ul>
+
+            </div>
+
+        </>
+    )
+}
+export default Metas
--- src/Views/Pokemon.jsx
+++ src/Views/Pokemon.jsx
@@ -7,6 +7,7 @@ import styles from '../Styles/Pokemon.module.scss'
 
 import Identity from '../Components/Identity/Identity';
 import Stats from '../Components/Stats/Stats';
+import Metas from '../Components/Metas/Metas';
 
 const Pokemon = () => {
 
@@ -26,39 +27,7 @@ const Pokemon = () => {
 
                             <Stats pokemon={pokemon} />
 
-                            <ul>
-                                
-                            </ul>
-
-                            <ul>
-                                { pokemon.abilities &&
-                                    <li>Abilities: { pokemon.abilities.map( (ability, index) =>
-                                        <span key={ index }>{ ability.ability.name }</span>
-                                    )}</li>
-                                }
-                                <li>Height: { (Math.round(pokemon.height * 10) / 100).toFixed(2) } m</li>
-                                <li>Weight: { (Math.round(pokemon.weight * 10) / 100).toFixed(2) } kg</li>
-                                <li>Catch rate: { pokemonDetails.capture_rate }</li>
-                                <li>Hatch counter: { pokemonDetails.hatch_counter } cycles</li>
-                                <li>Gender: 
-                                    { pokemonDetails.gender_rate === -1 ? 
-                                        <span>Unknown</span> :
-                                        <>
-                                        <span>{ (pokemonDetails.gender_rate / 8) * 100 }% female</span>
-                                        <span>{ ((8 - pokemonDetails.gender_rate) / 8) * 100 }% male</span>  
-                                        </>
-                                    }
-                                    
-                                </li>
-                                <li>
-                                    Group: 
-                                    { 
-                                        pokemonDetails.egg_groups?.map( (group, index) => {
-                                        return <span key={ index }>{ group.name }</span>
-                                        })
-                                    } 
-                                </li>
-                            </ul>
+                            <Metas pokemon={pokemon} pokemonDetails={pokemonDetails} />
 
                             { pokemon.species && 
                                 <Evolutions  />
